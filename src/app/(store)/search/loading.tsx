3ca4393a87eddb4a1e8530f8b35e2b6f1d982a3d
migueelzz <miguellemes005@gmail.com>
2024-09-25 22:02:13 -0300

    test: Create tests e2e with cypress

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,27 @@
+'use client'
+
+import { Skeleton } from '@/components/skeleton'
+import { useSearchParams } from 'next/navigation'
+
 export default function SearchLoading() {
-  return <div>carregando busca...</div>
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get('q')
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="flex flex-col gap-6 lg:grid lg:grid-cols-3">
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+      </div>
+    </div>
+  )
 }
--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,5 +1,72 @@
-export default async function Search() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+import { api } from '@/data/api'
+import { Product } from '@/data/types/products'
+import Image from 'next/image'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
-  return <h1>Search</h1>
+async function searchProducts(query: string): Promise<Product[]> {
+  const response = await api(`/products/search?q=${query}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
+
+  const products = await response.json()
+
+  return products
+}
+
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: { q: string }
+}) {
+  const { q: query } = searchParams
+
+  if (!query) {
+    redirect('/')
+  }
+
+  const products = await searchProducts(query)
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultado para: <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="flex flex-col gap-6 pb-8 lg:grid lg:grid-cols-3">
+        {products.map((product) => {
+          return (
+            <Link
+              key={product.id}
+              href={`/product/${product.slug}`}
+              className="group relative flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+            >
+              <Image
+                src={product.image}
+                className="transition-transform duration-500 group-hover:scale-105"
+                width={480}
+                height={480}
+                quality={100}
+                alt=""
+              />
+
+              <div className="absolute bottom-10 right-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+                <span className="truncate text-sm">{product.title}</span>
+                <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                  {product.price.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                    minimumFractionDigits: 0,
+                    maximumFractionDigits: 0,
+                  })}
+                </span>
+              </div>
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
